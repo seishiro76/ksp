299fda4c4d97682bd6290a0d956c08a9c99416d1
seishiro76 <snvrpv@gmail.com>
2026-04-20 19:45:53 +0700

    Написано view

--- config/main/views.py
+++ config/main/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import ConsultationForm
 
-# Create your views here.
+
+def consultation_view(request):
+    submitted_data = None
+
+    if request.method == "POST":
+        form = ConsultationForm(request.POST)
+        if form.is_valid():
+            submitted_data = form.cleaned_data
+    else:
+        form = ConsultationForm()
+
+    return render(request, "main/form_page.html", {
+        "form": form,
+        "submitted_data": submitted_data,
+    })
